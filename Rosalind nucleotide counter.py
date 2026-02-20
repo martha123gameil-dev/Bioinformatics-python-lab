9712bd0342ee6d9c0e87e2a92ea380ce5737db06
martha123gameil-dev <martha123gameil@gmail.com>
2026-02-21 00:02:23 +0200

    Add files via upload

---
+++ Rosalind nucleotide counter.py
@@ -0,0 +1,7 @@
+dna_sequence = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAGAGTGTCTGATAGCAGC"
+#  2. Count each nucleotide (Alphabetical order: A, C, G, T)
+a_count = dna_sequence.count("A")
+c_count = dna_sequence.count("C")
+g_count = dna_sequence.count("G")
+t_count = dna_sequence.count("T")
+print(a_count, c_count, g_count, t_count)
